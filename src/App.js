5a1d1eee7a5b768f206e0afb1840a87cf7727e12
zyhou <richard.maxime9@gmail.com>
2018-10-29 16:07:21 +0100

    Add logic + component board/square

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
 import { MainMenu } from './MainMenu';
+import { Board } from './Board';
+import { getInitBoard } from './game';
 
 const GameContext = React.createContext({
   message: '',
+  squares: [],
 });
 
 export const GameConsumer = GameContext.Consumer;
 const GameProvider = GameContext.Provider;
 
-export const App = () => (
-  <GameProvider
-    value={{
-      message: 'oui',
-    }}
-  >
-    <div className="game">
-      <div className="game-info">
-        <MainMenu />
-      </div>
-      <div className="game-board">Board</div>
-    </div>
-  </GameProvider>
-);
+export class App extends React.Component {
+  state = {
+    message: 'Morpion React',
+    squares: getInitBoard(),
+  };
+
+  render() {
+    const { message, squares } = this.state;
+    return (
+      <GameProvider
+        value={{
+          message,
+          squares,
+        }}
+      >
+        <div className="game">
+          <div className="game-info">
+            <MainMenu />
+          </div>
+          <div className="game-board">
+            <Board squares={squares} />
+          </div>
+        </div>
+      </GameProvider>
+    );
+  }
+}
---
+++ src/Board.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Square from './Square';
+
+export class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+        tabIndex={i}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+Board.propTypes = {
+  squares: PropTypes.arrayOf(PropTypes.number).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
